1253c8ddb1dad8ea559c37ac3575196b8a28092f
Emir Ayar <emirayar@amazon.lu>
2022-06-05 21:43:31 +0200

    use aws temporary creds

--- setup_cloud_service.sh
+++ setup_cloud_service.sh
@@ -8,9 +8,11 @@ mkdir deploy
 
 docker build -t $PROJECT_NAME -f setup_cloud_service-Dockerfile .
 docker run -it --rm \
-  -v $HOME/.aws:/root/.aws \
   -v $PWD/deploy:/deploy \
   -e PROJECT_NAME=$PROJECT_NAME \
   -e AWS_REGION=$AWS_REGION \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
   --name $PROJECT_NAME \
   --network host $PROJECT_NAME
